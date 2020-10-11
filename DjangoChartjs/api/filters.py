b7ea223e9fa6418ff2c0c116917e52ba57b1f588
Piotr Sobieszczyk <p.sobieszczyk1@gmail.com>
2020-10-11 23:06:53 +0200

    endpoints to api

--- DjangoChartjs/api/filters.py
+++ DjangoChartjs/api/filters.py
@@ -4,6 +4,9 @@ from . import models
 class GamesFilter(filter.FilterSet):
     min_price = filter.NumberFilter(field_name = 'price', lookup_expr = 'gte')
     max_price = filter.NumberFilter(field_name = 'price', lookup_expr = 'lte')
+    start_date = filter.DateFilter(field_name='release_date', lookup_expr='gt') 
+    end_date = filter.DateFilter(field_name='release_date', lookup_expr='lt')
+    #date_range = filter.DateRangeFilter(field_name='release_date')
 
     class Meta:
         model = models.Games
